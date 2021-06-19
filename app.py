905af4ec5ad5aaf60684d34c05ea4fe841a6b52a
arkarimi <79075734+arkarimi@users.noreply.github.com>
2021-06-19 16:08:18 +0430

    Create app.py

---
+++ app.py
@@ -0,0 +1,246 @@
+# alphabet translate
+import datetime
+
+# dateTime variables
+now = datetime.datetime.now()
+hour = now.hour
+minute = now.minute
+
+
+endnum = ''
+
+# main calculations
+
+y = 1
+x = ( hour + minute ) * 42
+end = ( y * x ) + 36
+
+
+
+# alphabet number
+a = 1
+b = 2
+c = 3
+d = 4
+e = 5
+f = 6
+g = 7
+h = 8
+i = 9
+j = 10
+k = 11
+l = 12
+m = 13
+n = 14
+o = 15
+p = 16
+q = 17
+r = 18
+s = 19
+t = 20
+u = 21
+v = 22
+w = 23
+x = 24
+y = 25
+z = 26
+
+
+# Conditions and ending
+test = input('Enter your text to encrypt : ')
+listx = []
+
+for ii in test:
+	listx.append(ii)
+
+	if 'a' in listx:
+		y = a
+		x = ( hour + minute ) * 42
+		end = ( y * x ) + 36
+
+		endnum += str(end)
+
+	if 'b' in listx:
+		y = b
+		x = ( hour + minute ) * 42
+		end = ( y * x ) + 36
+
+		endnum += str(end)
+
+	if 'c' in listx:
+		y = c
+		x = ( hour + minute ) * 42
+		end = ( y * x ) + 36
+
+		endnum += str(end)
+
+	if 'd' in listx:
+		y = d
+		x = ( hour + minute ) * 42
+		end = ( y * x ) + 36
+
+		endnum += str(end)
+
+	if 'e' in listx:
+		y = e
+		x = ( hour + minute ) * 42
+		end = ( y * x ) + 36
+
+		endnum += str(end)
+
+	if 'f' in listx:
+		y = f
+		x = ( hour + minute ) * 42
+		end = ( y * x ) + 36
+
+		endnum += str(end)
+
+	if 'g' in listx:
+		y = g
+		x = ( hour + minute ) * 42
+		end = ( y * x ) + 36
+
+		endnum += str(end)
+
+	if 'h' in listx:
+		y = h
+		x = ( hour + minute ) * 42
+		end = ( y * x ) + 36
+
+		endnum += str(end)
+
+	if 'i' in listx:
+		y = i
+		x = ( hour + minute ) * 42
+		end = ( y * x ) + 36
+
+		endnum += str(end)
+
+	if 'j' in listx:
+		y = j
+		x = ( hour + minute ) * 42
+		end = ( y * x ) + 36
+
+		endnum += str(end)
+
+	if 'k' in listx:
+		y = k
+		x = ( hour + minute ) * 42
+		end = ( y * x ) + 36
+
+		endnum += str(end)
+
+	if 'l' in listx:
+		y = l
+		x = ( hour + minute ) * 42
+		end = ( y * x ) + 36
+
+		endnum += str(end)
+
+	if 'm' in listx:
+		y = m
+		x = ( hour + minute ) * 42
+		end = ( y * x ) + 36
+
+		endnum += str(end)
+
+	if 'n' in listx:
+		y = n
+		x = ( hour + minute ) * 42
+		end = ( y * x ) + 36
+
+		endnum += str(end)
+
+	if 'o' in listx:
+		y = o
+		x = ( hour + minute ) * 42
+		end = ( y * x ) + 36
+
+		endnum += str(end)
+
+	if 'p' in listx:
+		y = p
+		x = ( hour + minute ) * 42
+		end = ( y * x ) + 36
+
+		endnum += str(end)
+
+	if 'q' in listx:
+		y = q
+		x = ( hour + minute ) * 42
+		end = ( y * x ) + 36
+
+		endnum += str(end)
+
+	if 'r' in listx:
+		y = r
+		x = ( hour + minute ) * 42
+		end = ( y * x ) + 36
+
+		endnum += str(end)
+
+	if 's' in listx:
+		y = s
+		x = ( hour + minute ) * 42
+		end = ( y * x ) + 36
+
+		endnum += str(end)
+
+	if 't' in listx:
+		y = t
+		x = ( hour + minute ) * 42
+		end = ( y * x ) + 36
+
+		endnum += str(end)
+
+	if 'u' in listx:
+		y = u
+		x = ( hour + minute ) * 42
+		end = ( y * x ) + 36
+
+		endnum += str(end)
+
+	if 'v' in listx:
+		y = v
+		x = ( hour + minute ) * 42
+		end = ( y * x ) + 36
+
+		endnum += str(end)
+
+	if 'w' in listx:
+		y = w
+		x = ( hour + minute ) * 42
+		end = ( y * x ) + 36
+
+		endnum += str(end)
+	if 'x' in listx:
+		y = x
+		x = ( hour + minute ) * 42
+		end = ( y * x ) + 36
+
+		endnum += str(end)
+
+	if 'y' in listx:
+		y = y
+		x = ( hour + minute ) * 42
+		end = ( y * x ) + 36
+
+		endnum += str(end)
+
+	if 'z' in listx:
+		y = z
+		x = ( hour + minute ) * 42
+		end = ( y * x ) + 36
+
+		endnum += str(end)
+
+
+
+
+
+print(endnum)
+print(minute) 
+
+	
+
+	
